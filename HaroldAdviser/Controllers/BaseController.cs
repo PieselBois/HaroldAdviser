e0c799126d420205780ec1dfa2854fba06556a96
Ravil Bikbulatov <weeellz12@gmail.com>
2018-04-11 21:04:00 +0300

    add github oauth

---
+++ HaroldAdviser/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using HaroldAdviser.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Authentication;
+using System.Security.Claims;
+
+namespace HaroldAdviser.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected GithubUser GetUser()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                throw new AuthenticationException("User not autheticated");
+            }
+
+            var model = new GithubUser
+            {
+                Name = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value,
+                Login = User.FindFirst(c => c.Type == "urn:github:login")?.Value,
+                Url = User.FindFirst(c => c.Type == "urn:github:url")?.Value
+            };
+
+            return model;
+        }
+    }
+}
