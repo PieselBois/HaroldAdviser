85e1fdb30ff0aa6854c981581df618c7ed243c4f
Ravil Bikbulatov <weeellz12@gmail.com>
2018-04-11 19:57:52 +0300

    create project

---
+++ HaroldAdviser.Tests/TravisTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace HaroldAdviser.Tests
+{
+    public class TravisTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.True(true);
+        }
+    }
+}
