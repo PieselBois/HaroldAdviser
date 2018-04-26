9e139ed6a6d2fcc3bead0eb2bc3f12d71afcd7f6
Ravil Bikbulatov <weeellz12@gmail.com>
2018-04-26 21:17:57 +0300

    HA-19 Add dotnet-ef tool

--- HaroldAdviser/Migrations/20180426181239_AddCheckedInRepo.Designer.cs
+++ HaroldAdviser/Migrations/20180426181239_AddCheckedInRepo.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace HaroldAdviser.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20180426181239_AddCheckedInRepo")]
+    partial class AddCheckedInRepo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HaroldAdviser/Migrations/20180426181239_AddCheckedInRepo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HaroldAdviser.Migrations
+{
+    public partial class AddCheckedInRepo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Checked",
+                table: "Repositories",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Checked",
+                table: "Repositories");
+        }
+    }
+}
