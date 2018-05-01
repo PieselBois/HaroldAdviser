980c748a106a7cc6863e293dbe60c3fc07db4087
Ravil Bikbulatov <weeellz12@gmail.com>
2018-05-02 00:17:09 +0300

    fix

--- HaroldAdviser/Controllers/UserController.cs
+++ HaroldAdviser/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 using HaroldAdviser.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
